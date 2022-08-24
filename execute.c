86ae49591c4921f7d3f605cd60799f1911991fa9
NekodaMushi <4252@holbertonschool.com>
2022-08-24 14:17:58 +0000

    Reducing nb of line per file

---
+++ execute.c
@@ -0,0 +1,28 @@
+/**
+ * execute - Check and do instruction from file
+ * @stack: the linked list
+ * @token: token used as command
+ * @line_number: number of line
+ * Return: Nothing
+ */
+void execute(ostack_t **stack, char *token, unsigned int line_number)
+{
+	int i = 0;
+	instruction_t search[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}};
+
+	if (!token)
+		return;
+	while (search[i].opcode != NULL)
+	{
+		if (strcmp(search[i].opcode, token) == 0)
+		{
+			search[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	error_handle("L%d: unknown instruction %s\n", line_number, token);
+}
